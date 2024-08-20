7127114e56508c06b9d66c2860ee772d30aa1f71
harsh-311 <harshpatel3671@gmail.com>
2024-08-20 10:05:36 +0530

    IIFE functions in javascripts.

--- javascript/one.js
+++ javascript/one.js
@@ -1,1 +1,15 @@
-console.log("Hello Harsh")
+// IIFE function...
+
+(function(){
+    console.log("Hello World")
+})();
+
+(()=>console.log("Hello world"))();
+
+// With parameters
+(function(name){
+    console.log(`My name is ${name}`)
+})("Harsh Patel");
+
+((name)=>console.log(`My name is ${name}`))("Harsh Patel")
+
